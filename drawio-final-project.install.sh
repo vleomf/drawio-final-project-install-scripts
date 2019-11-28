7ae35a188fa5ae28696033b3214560896f3e5ab1
Leopoldo Muñoz <viceo00@gmail.com>
2019-11-28 00:58:36 -0600

    Update drawio-final-project.install.sh

--- drawio-final-project.install.sh
+++ drawio-final-project.install.sh
@@ -9,3 +9,5 @@ echo
 git clone https://github.com/viceo/drawio.git
 git clone https://github.com/viceo/drawio-codegen-microservice.git
 git clone https://github.com/viceo/drawio-nginx-gateway.git
+docker-compose build
+docker-compose up -d
