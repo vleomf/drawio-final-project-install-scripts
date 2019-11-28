23222e90ec215acda18e040a3a514cb0e435fde0
Leopoldo Muñoz <viceo00@gmail.com>
2019-11-27 23:01:21 -0600

    eliminando cadenas olvidadas

--- drawio-final-project.install.sh
+++ drawio-final-project.install.sh
@@ -3,9 +3,9 @@ echo "Clonando proyecto..."
 echo
 git clone https://github.com/viceo/drawio-final-project.git
 cd drawio-final-project
-echo "\n"
+echo
 echo "Clonando servicios..."
-echo "\n\n"
+echo
 git clone https://github.com/viceo/drawio.git
 git clone https://github.com/viceo/drawio-codegen-microservice.git
 git clone https://github.com/viceo/drawio-nginx-gateway.git
