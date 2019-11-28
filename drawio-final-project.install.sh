4ffc269102469777f02c2a0ab697ebb7d675f526
Leopoldo Muñoz <viceo00@gmail.com>
2019-11-27 22:46:26 -0600

    agregando formato

--- drawio-final-project.install.sh
+++ drawio-final-project.install.sh
@@ -1,10 +1,13 @@
+echo "\n"
 echo "Clonando proyecto..."
-
+echo "\n\n"
 git clone https://github.com/viceo/drawio-final-project.git
 
 cd drawio-final-project
 
+echo "\n"
 echo "Clonando servicios..."
+echo "\n\n"
 
 git clone https://github.com/viceo/drawio.git
 git clone https://github.com/viceo/drawio-codegen-microservice.git
