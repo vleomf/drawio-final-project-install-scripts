0fa9cb10cf40c5b83862fd538ba6623ba1910636
Leopoldo Muñoz <viceo00@gmail.com>
2019-11-27 22:07:45 -0600

    Subiendo script instalación Linux/Mac

---
+++ drawio-final-project.install.sh
@@ -0,0 +1,11 @@
+echo "Clonando proyecto..."
+
+git clone https://github.com/viceo/drawio-final-project.git
+
+cd drawio-final-project
+
+echo "Clonando servicios..."
+
+git clone https://github.com/viceo/drawio.git
+git clone https://github.com/viceo/drawio-codegen-microservice.git
+git clone https://github.com/viceo/drawio-nginx-gateway.git
