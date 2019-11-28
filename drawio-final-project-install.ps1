4f7f4995f7b410ff9ba8babfabf865d396e102e5
Leopoldo Muñoz <viceo00@gmail.com>
2019-11-27 23:33:22 -0600

    Subiendo script para generar proyecto en windows

---
+++ drawio-final-project-install.ps1
@@ -0,0 +1,11 @@
+echo ""
+echo "Clonando proyecto..."
+echo ""
+git clone https://github.com/viceo/drawio-final-project.git
+cd drawio-final-project
+echo ""
+echo "Clonando servicios..."
+echo ""
+git clone https://github.com/viceo/drawio.git
+git clone https://github.com/viceo/drawio-codegen-microservice.git
+git clone https://github.com/viceo/drawio-nginx-gateway.git
